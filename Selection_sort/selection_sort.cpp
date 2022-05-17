80fe57cc49d038b371fd73dbecf8be2e8d4f1d99
Pixel-0xff <ht19a022@oecu.jp>
2022-05-18 01:26:20 +0900

    プロジェクト ファイルを追加します。

---
+++ Selection_sort/selection_sort.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	int arr[] = { 2,1,9,5,3,6,8,4,0,7 };
+	size_t MAX_INDEX = sizeof(arr) / sizeof(int);
+
+	cout << "selection_sort" << endl;
+
+
+
+	for (int i = 0; i < MAX_INDEX; i++) {
+		int max = arr[0]; size_t index = 0;
+
+		for (int j = 0; j < MAX_INDEX - i; j++) {
+			if (arr[j] > max) max = arr[j], index = j;
+
+		}
+		int tmp = arr[MAX_INDEX - i - 1];
+		arr[MAX_INDEX - i - 1] = max;
+		arr[index] = tmp;
+	}
+
+	for (const int x : arr) cout << x << " ";
+
+}
---
+++ bubble_sort/bubble_sort.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	int arr[] = { 2,1,9,5,3,6,8,4,0,7 };
+	size_t MAX_INDEX = sizeof(arr) / sizeof(int);
+
+	cout << "bubble_sort" << endl;
+
+	for (int i = 0; i < MAX_INDEX; i++) {
+		for (int j = 0; j < MAX_INDEX - i - 1; j++) {
+
+			if (arr[j] > arr[j + 1]) {
+				int tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+
+		}
+	}
+
+	for (const int x : arr) cout << x << " ";
+
+}
