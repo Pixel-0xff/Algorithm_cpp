33cea132838e83efbd5c524fff8564e20f555e1c
Pixel-0xff <ht19a022@oecu.jp>
2022-05-25 23:13:07 +0900

    bubble_sort

--- bubble_sort/bubble_sort.cpp
+++ bubble_sort/bubble_sort.cpp
@@ -7,9 +7,10 @@ int main()
 	int arr[] = { 2,1,9,5,3,6,8,4,0,7 };
 	size_t MAX_INDEX = sizeof(arr) / sizeof(int);
 
-	cout << "bubble_sort" << endl;
+	cout << "---bubble_sort---" << endl;
 
 	for (int i = 0; i < MAX_INDEX; i++) {
+
 		for (int j = 0; j < MAX_INDEX - i - 1; j++) {
 
 			if (arr[j] > arr[j + 1]) {
@@ -17,10 +18,10 @@ int main()
 				arr[j] = arr[j + 1];
 				arr[j + 1] = tmp;
 			}
-
 		}
 	}
 
 	for (const int x : arr) cout << x << " ";
 
+	return 0;
 }
